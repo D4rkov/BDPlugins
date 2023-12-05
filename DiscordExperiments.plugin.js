21169d2ea79379ec9155b64e0edb066ffabf71f6
Darkov <83168801+D4rkov@users.noreply.github.com>
2023-12-05 22:06:52 +0200

    Add files via upload

---
+++ DiscordExperiments.plugin.js
@@ -0,0 +1,43 @@
+/**
+ * @name DiscordExperiments
+ * @description Enables the experiments tab in discord's settings.
+ * @author Darkov, Zyrenth, MeguminSama
+ * @version 1.0.0
+ * @source https://github.com/D4rkov/BDPlugins/blob/main/discordexperiments.plugin.js
+ * @updateUrl https://raw.githubusercontent.com/D4rkov/BDPlugins/main/discordexperiments.plugin.js
+ */
+
+let cache; webpackChunkdiscord_app.push([["wp_isdev_patch"], {}, r => cache=r.c]);
+var UserStore = Object.values(cache).find(m => m?.exports?.default?.getUsers).exports.default;
+var actions = Object.values(UserStore._dispatcher._actionHandlers._dependencyGraph.nodes);
+var user = UserStore.getCurrentUser();
+
+module.exports = class {
+  getName(){
+    return "Discord Experiments";
+  }
+
+  start() {
+    actions.find(n => n.name === "ExperimentStore").actionHandler.CONNECTION_OPEN({
+      type: "CONNECTION_OPEN", 
+      user: {flags: user.flags |= 1}, 
+      experiments: [],
+    });
+    actions.find(n => n.name === "DeveloperExperimentStore").actionHandler.CONNECTION_OPEN();
+    webpackChunkdiscord_app.pop(); 
+    user.flags &= ~1; 
+    return "Experiments Enabled";
+  }
+
+  stop(){
+    actions.find(n => n.name === "ExperimentStore").actionHandler.CONNECTION_OPEN({
+      type: "CONNECTION_OPEN", 
+      user: {flags: user.flags |= 0}, 
+      experiments: [],
+    });
+    actions.find(n => n.name === "DeveloperExperimentStore").actionHandler.CONNECTION_OPEN();
+    webpackChunkdiscord_app.pop(); 
+    user.flags &= ~0; 
+    return "Experiments Disabled";
+  }
+};
