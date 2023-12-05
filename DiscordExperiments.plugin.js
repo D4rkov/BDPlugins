4174da0bdd0882bde15bca99dade46b83d6a48e4
Darkov <83168801+D4rkov@users.noreply.github.com>
2023-12-05 22:12:46 +0200

    Update DiscordExperiments.plugin.js

--- DiscordExperiments.plugin.js
+++ DiscordExperiments.plugin.js
@@ -2,7 +2,7 @@
  * @name DiscordExperiments
  * @description Enables the experiments tab in discord's settings.
  * @author Darkov, Zyrenth, MeguminSama
- * @version 1.0.0
+ * @version 1.0.1
  * @source https://github.com/D4rkov/BDPlugins/blob/main/DiscordExperiments.plugin.js
  * @updateUrl https://raw.githubusercontent.com/D4rkov/BDPlugins/main/DiscordExperiments.plugin.js
  */
