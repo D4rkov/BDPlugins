fbe4fd41ac72581628d5e2984834f59bf4a97af3
Darkov <83168801+D4rkov@users.noreply.github.com>
2024-07-26 15:57:53 +0300

    Update DiscordExperiments.plugin.js

--- DiscordExperiments.plugin.js
+++ DiscordExperiments.plugin.js
@@ -3,27 +3,39 @@
  * @description Enables the experiments tab in discord's settings.
  * @author Darkov, Zyrenth, MeguminSama
  * @source https://github.com/D4rkov/BDPlugins/blob/main/DiscordExperiments.plugin.js
- * @version 1.0.0
+ * @version 1.0.1
  */
 
-let cache; webpackChunkdiscord_app.push([["wp_isdev_patch"], {}, r => cache=r.c]);
-var UserStore = Object.values(cache).find(m => m?.exports?.default?.getUsers).exports.default;
-var actions = Object.values(UserStore._dispatcher._actionHandlers._dependencyGraph.nodes);
-var user = UserStore.getCurrentUser();
+const cache = {};
+webpackChunkdiscord_app.push([["wp_isdev_patch"], {}, r => Object.assign(cache, r.c)]);
+const UserStore = Object.values(cache).find(m => m?.exports?.default?.getUsers).exports.default;
+const actions = Object.values(UserStore._dispatcher._actionHandlers._dependencyGraph.nodes);
+const user = UserStore.getCurrentUser();
+
+let intervalId = null;
 
 module.exports = class {
 
   start() {
-    user.flags &= ~0;
-    actions.find(n => n.name === "ExperimentStore").actionHandler.CONNECTION_OPEN({
-      type: "CONNECTION_OPEN", 
-      user: {flags: user.flags |= 1}, 
-      experiments: [],
-    });
-    actions.find(n => n.name === "DeveloperExperimentStore").actionHandler.CONNECTION_OPEN();
+    if (!intervalId) {
+      intervalId = setInterval(() => {
+        user.flags &= ~0;
+        actions.find(n => n.name === "ExperimentStore").actionHandler.CONNECTION_OPEN({
+          type: "CONNECTION_OPEN", 
+          user: {flags: user.flags |= 1}, 
+          experiments: [],
+        });
+        actions.find(n => n.name === "DeveloperExperimentStore").actionHandler.CONNECTION_OPEN();
+      }, 5000);
+    }
   }
 
-  stop(){
+  stop() {
+    if (intervalId) {
+      clearInterval(intervalId);
+      intervalId = null;
+    }
+    
     user.flags &= ~1;
     actions.find(n => n.name === "ExperimentStore").actionHandler.CONNECTION_OPEN({
       type: "CONNECTION_OPEN", 
@@ -32,4 +44,5 @@ module.exports = class {
     });
     actions.find(n => n.name === "DeveloperExperimentStore").actionHandler.CONNECTION_OPEN();
   }
+
 };
