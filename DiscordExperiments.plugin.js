8456c5b41be9a4286a5da74f7e8aefb739cd9341
Darkov <83168801+D4rkov@users.noreply.github.com>
2025-06-03 21:25:00 +0300

    Update DiscordExperiments.plugin.js
    Best Discord Experiments plugin there is!

--- DiscordExperiments.plugin.js
+++ DiscordExperiments.plugin.js
@@ -1,49 +1,87 @@
 /**
  * @name DiscordExperiments
- * @description Enables the experiments tab in discord's settings.
- * @author Darkov, Zyrenth, MeguminSama
+ * @description Enables the experiments tab in Discord's settings.
+ * @author Darkov, Zyrenth, MeguminSama, VincentX0905(炸蝦)
  * @source https://github.com/D4rkov/BDPlugins/blob/main/DiscordExperiments.plugin.js
  * @updateUrl https://raw.githubusercontent.com/D4rkov/BDPlugins/main/DiscordExperiments.plugin.js
- * @version 1.0.1
+ * @version 1.0.2
  */
-
-const cache = {};
-webpackChunkdiscord_app.push([["wp_isdev_patch"], {}, r => Object.assign(cache, r.c)]);
-const UserStore = Object.values(cache).find(m => m?.exports?.default?.getUsers).exports.default;
-const actions = Object.values(UserStore._dispatcher._actionHandlers._dependencyGraph.nodes);
-const user = UserStore.getCurrentUser();
 
 let intervalId = null;
 
-module.exports = class {
+function getUserModule() {
+    let cache = webpackChunkdiscord_app.push([[Symbol()], {}, r => r.c]);
+    webpackChunkdiscord_app.pop();
+    return Object.values(cache).find(
+        x => x?.exports?.default?.__proto__?.getUsers && x?.exports?.default?.getCurrentUser
+    )?.exports?.default;
+}
 
-  start() {
-    if (!intervalId) {
-      intervalId = setInterval(() => {
-        user.flags &= ~0;
-        actions.find(n => n.name === "ExperimentStore").actionHandler.CONNECTION_OPEN({
-          type: "CONNECTION_OPEN", 
-          user: {flags: user.flags |= 1}, 
-          experiments: [],
-        });
-        actions.find(n => n.name === "DeveloperExperimentStore").actionHandler.CONNECTION_OPEN();
-      }, 5000);
+function getActionHandlers(userModule) {
+    return userModule?._dispatcher?._actionHandlers?._dependencyGraph?.nodes
+        ? Object.values(userModule._dispatcher._actionHandlers._dependencyGraph.nodes)
+        : [];
+}
+
+function isExperimentsEnabled() {
+    try {
+        let userModule = getUserModule();
+        return (userModule.getCurrentUser().flags & 1) === 1;
+    } catch {
+        return false;
     }
-  }
+}
 
-  stop() {
-    if (intervalId) {
-      clearInterval(intervalId);
-      intervalId = null;
+async function applyPatch() {
+    try {
+        let userModule = getUserModule();
+        if (!userModule) throw new Error("User module not found");
+        userModule.getCurrentUser().flags |= 1;
+        const actionHandlers = getActionHandlers(userModule);
+        const devExpStore = actionHandlers.find(x => x.name === "DeveloperExperimentStore");
+        devExpStore?.actionHandler?.["CONNECTION_OPEN"]?.();
+        try {
+            actionHandlers.find(x => x.name === "ExperimentStore")
+                ?.actionHandler?.["OVERLAY_INITIALIZE"]?.({ user: { flags: 1 } });
+        } catch {}
+        actionHandlers.find(x => x.name === "ExperimentStore")?.storeDidChange();
+    } catch (error) {
+        console.log(error);
+        BdApi.UI.showNotice("An error occurred with the DiscordExperiments plugin", {type: "error"});
     }
-    
-    user.flags &= ~1;
-    actions.find(n => n.name === "ExperimentStore").actionHandler.CONNECTION_OPEN({
-      type: "CONNECTION_OPEN", 
-      user: {flags: user.flags |= 0}, 
-      experiments: [],
-    });
-    actions.find(n => n.name === "DeveloperExperimentStore").actionHandler.CONNECTION_OPEN();
-  }
+}
 
-};
+async function revertPatch() {
+    try {
+        let userModule = getUserModule();
+        if (!userModule) throw new Error("User module not found");
+        userModule.getCurrentUser().flags &= ~1;
+        const actionHandlers = getActionHandlers(userModule);
+        const devExpStore = actionHandlers.find(x => x.name === "DeveloperExperimentStore");
+        devExpStore?.actionHandler?.["CONNECTION_OPEN"]?.();
+        try {
+            actionHandlers.find(x => x.name === "ExperimentStore")
+                ?.actionHandler?.["OVERLAY_INITIALIZE"]?.({ user: { flags: 0 } });
+        } catch {}
+        actionHandlers.find(x => x.name === "ExperimentStore")?.storeDidChange();
+    } catch (error) {
+        console.log(error);
+        BdApi.UI.showNotice("An error occurred while reverting DiscordExperiments", {type: "error"});
+    }
+}
+
+module.exports = class DiscordExperiments {
+    async start() {
+        await applyPatch();
+        intervalId = setInterval(async () => {
+            if (!isExperimentsEnabled()) {
+                await applyPatch();
+            }
+        }, 2500);
+    }
+
+    async stop() {
+        if (intervalId) clearInterval(intervalId);
+        await revertPatch();
+    }
+}
